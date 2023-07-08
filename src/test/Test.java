907a2001d4d590c58c6b1c4f97f3da1573f86e01
Péťa <Pa@192.168.0.15>
2023-07-08 16:57:39 +0200

    xxx

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test4");
 		System.out.println("test1");
-		System.out.println("test2");
 		System.out.println("test3");
 		System.out.println("test4");
 
