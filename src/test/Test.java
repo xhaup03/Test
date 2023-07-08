0f3b3d5e431791f99553e70e0073e26906e49183
Péťa <Pa@192.168.0.15>
2023-07-08 16:56:14 +0200

    really last commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,7 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("test4");
 		System.out.println("test1");
 		System.out.println("test2");
 		System.out.println("test3");
